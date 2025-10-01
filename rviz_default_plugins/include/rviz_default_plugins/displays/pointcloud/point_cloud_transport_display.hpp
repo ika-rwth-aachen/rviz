40c6d5b038d5d4befb883158020c444ad82e7d16
Emre Karincali <emre.karincali@ika.rwth-aachen.de>
2025-10-01 10:26:05 +0000

    fix typo

--- rviz_default_plugins/include/rviz_default_plugins/displays/pointcloud/point_cloud_transport_display.hpp
+++ rviz_default_plugins/include/rviz_default_plugins/displays/pointcloud/point_cloud_transport_display.hpp
@@ -60,7 +60,7 @@ public:
     QString message_type = QString::fromStdString(rosidl_generator_traits::name<MessageType>());
     std::vector<QString> supported_types = {
       "sensor_msgs/msg/PointCloud2",
-      "point_cloud_intefaces/msg/CompressedPointCloud2"
+      "point_cloud_interfaces/msg/CompressedPointCloud2"
     };
       
     topic_property_->setMessageTypes(supported_types);
